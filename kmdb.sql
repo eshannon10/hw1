b801fdcddb041e0ab0af2b989bdf6d6957123c5d
Ellie Shannon <ellie.shannon@kellogg.northwestern.edu>
2025-01-26 17:11:42 +0000

    added the data and printed output (done)

--- kmdb.sql
+++ kmdb.sql
@@ -157,7 +157,45 @@ CREATE TABLE roles (
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+
+-- Studios
+INSERT INTO studios (studio_name) VALUES ("Warner Bros.");
+
+-- Movies
+INSERT INTO movies (title, year, rating, studio_id) VALUES ("Batman Begins", 2005, "PG-13", 1);
+INSERT INTO movies (title, year, rating, studio_id) VALUES ("The Dark Knight", 2008, "PG-13", 1);
+INSERT INTO movies (title, year, rating, studio_id) VALUES ("The Dark Knight Rises", 2012, "PG-13", 1);
+
+-- Actors
+INSERT INTO actors (actor_name) VALUES ("Christian Bale");
+INSERT INTO actors (actor_name) VALUES ("Michael Caine");
+INSERT INTO actors (actor_name) VALUES ("Liam Neeson");
+INSERT INTO actors (actor_name) VALUES ("Katie Holmes");
+INSERT INTO actors (actor_name) VALUES ("Gary Oldman");
+INSERT INTO actors (actor_name) VALUES ("Heath Ledger");
+INSERT INTO actors (actor_name) VALUES ("Aaron Eckhart");
+INSERT INTO actors (actor_name) VALUES ("Maggie Gyllenhaal");
+INSERT INTO actors (actor_name) VALUES ("Tom Hardy");
+INSERT INTO actors (actor_name) VALUES ("Joseph Gordon-Levitt");
+INSERT INTO actors (actor_name) VALUES ("Anne Hathaway");
+
+-- Roles
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Bruce Wayne", 1, 1);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Alfred", 1, 2);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Ra's Al Ghul", 1, 3);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Rachel Dawes", 1, 4);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Commissioner Gordon", 1, 5);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Bruce Wayne", 2, 1);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Joker", 2, 6);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Harvey Dent", 2, 7);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Alfred", 2, 2);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Rachel Dawes", 2, 8);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Bruce Wayne", 3, 1);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Commissioner Gordon", 3, 5);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Bane", 3, 9);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("John Blake", 3, 10);
+INSERT INTO roles (character, movie_id, actor_id) VALUES ("Selina Kyle", 3, 11);
+
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -165,7 +203,17 @@ CREATE TABLE roles (
 .print ""
 
 -- The SQL statement for the movies output
--- TODO!
+
+SELECT 
+    movies.title,
+    movies.year,
+    movies.rating,
+    studios.studio_name
+FROM 
+    movies
+INNER JOIN 
+    studios ON movies.studio_id = studios.id;
+
 
 -- Prints a header for the cast output
 .print ""
@@ -175,4 +223,13 @@ CREATE TABLE roles (
 
 
 -- The SQL statement for the cast output
--- TODO!
+SELECT 
+    movies.title,
+    actors.actor_name,
+    roles.character
+FROM 
+    roles
+INNER JOIN 
+    movies ON roles.movie_id = movies.id
+INNER JOIN 
+    actors ON roles.actor_id = actors.id;
